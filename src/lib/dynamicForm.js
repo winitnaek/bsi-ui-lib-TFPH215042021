d5884357923fa3708838f8f6ad6aa45f1f8435dd
Raj Dangi <rdangi@bsihq.com>
2020-06-19 12:48:32 -0400

    Handle dependent field auto load

--- src/lib/dynamicForm.js
+++ src/lib/dynamicForm.js
@@ -1,163 +1,203 @@
-import React, { Component } from "react";
-import { Formik, Form} from "formik";
-import { Col, Button, Row, Label,Container, ModalBody, ModalFooter,FormGroup} from "reactstrap";
-import { updateGrid } from "./utils/updateGrid.js";
-import Input from "./inputTypes/input";
-import Select from "./inputTypes/select";
-import Radio from "./inputTypes/radio";
-import Checkbox from "./inputTypes/checkbox";
-import Date from "./inputTypes/date";
-import FileUpload from "./inputTypes/fileUpload";
-import Usage from "./usage";
-import { createYupSchema } from "./utils/createYupSchema";
-import * as yup from "yup";
+import React, { Component } from 'react';
+import { Formik, Form } from 'formik';
+import { Col, Button, Row, Label, Container, ModalBody, ModalFooter, FormGroup } from 'reactstrap';
+import { updateGrid } from './utils/updateGrid.js';
+import Input from './inputTypes/input';
+import Select from './inputTypes/select';
+import Radio from './inputTypes/radio';
+import Checkbox from './inputTypes/checkbox';
+import Date from './inputTypes/date';
+import FileUpload from './inputTypes/fileUpload';
+import Usage from './usage';
+import { createYupSchema } from './utils/createYupSchema';
+import * as yup from 'yup';
 
 class DynamicForm extends Component {
   constructor(props) {
     super(props);
+    const { fieldData } = props;
     this.state = {
       showDelete: false,
       isReset: false,
-      disabledFields:[]
+      disabledFields: [],
+      fieldData
     };
+
     this.handleView = () => {
       const { formProps, renderGrid } = this.props;
       const { pgid } = formProps;
       let data = this.props.tftools.filter(tftool => {
         if (tftool.id == pgid) return tftool;
       });
-      renderGrid(data[0])
+      renderGrid(data[0]);
     };
 
     this.handleReset = () => {
       this.setState({
         isReset: true
-      })
-    }
+      });
+    };
 
     this.renderMe = (pgid, values, filter) => {
       this.props.renderMe(pgid, values, filter);
-    }
+    };
 
     this.onDisableField = fields => {
       this.setState({
         disabledFields: fields
       });
-    }
+    };
+
+    this.updateFieldData = this.updateFieldData.bind(this);
   }
 
-  
-
   disabledHandler(id) {
-    const {disabledFields} = this.state;
-    const {formMetaData, formProps} = this.props;
+    const { disabledFields } = this.state;
+    const { formMetaData, formProps } = this.props;
     try {
       let row = disabledFields.filter(r => id == r);
-      if(row.length > 0) return true;
+      if (row.length > 0) return true;
       let formflds = formMetaData.formdef.formflds;
       if (formflds) {
         row = formflds.filter(r => id == r.id);
         if (row.length > 0) {
-          if (row[0].isReadOnlyOnEdit == true && this.props.formData.mode == "Edit") {
+          if (row[0].isReadOnlyOnEdit == true && this.props.formData.mode == 'Edit') {
             return true;
-          } else if (
-            row[0].isReadOnlyOnNew == true &&
-            this.props.formData.mode != "Edit"
-          ) {
+          } else if (row[0].isReadOnlyOnNew == true && this.props.formData.mode != 'Edit') {
             return true;
           }
         }
       }
       return false;
     } catch (error) {
-      console.log("error", error);
+      console.log('error', error);
     }
   }
 
+  updateFieldData(fieldId, options) {
+    const { fieldData } = this.state;
+    const updatedFieldData = fieldData.map(field => {
+      if (field.id === fieldId && field.fieldinfo && field.fieldinfo.options) {
+        field.fieldinfo.options = options;
+      }
+      return field;
+    });
+    this.setState({
+      fieldData: updatedFieldData
+    });
+  }
+
   renderFormElements(props, fieldInfo, autoComplete) {
-    if(this.state.isReset) {
+    if (this.state.isReset) {
       this.setState({
         isReset: false
-      })
+      });
     }
 
     return fieldInfo.map((item, index) => {
-      const fieldMap = { 
-        text:Input,
-        date:Date,
-        select:Select,
-        checkbox:Checkbox,
-        radio:Radio,
-        fileUpload:FileUpload,
+      const fieldMap = {
+        text: Input,
+        date: Date,
+        select: Select,
+        checkbox: Checkbox,
+        radio: Radio,
+        fileUpload: FileUpload
       };
       const Component = fieldMap[item.fieldtype];
       let error = props.errors.hasOwnProperty(item.id) && props.errors[item.id];
       let touched = props.touched.hasOwnProperty(item.id) && props.touched[item.id];
       if (item.fieldtype) {
-            return (
-              <Component
-                index={index}
-                fieldHeader={item.fieldHeader}
-                type={item.fieldtype}
-                label={item.label}
-                fieldinfo={item.fieldinfo && item.fieldinfo}
-                name={item.id}
-                id={item.id}
-                placeholder={item.placeholder}
-                description={item.description}
-                disabled={this.disabledHandler(item.id)}
-                onDisableField={this.onDisableField}
-                fieldsToDisable={item.disable}
-                value={props.values[item.id]}
-                required={item.validation && item.validation.required}
-                autoComplete={autoComplete}
-                onChange={item.fieldinfo && item.fieldinfo.typeahead ? props.setFieldValue : props.handleChange}
-                onBlur={props.handleBlur}
-                error={error}
-                touched={touched}
-                isReset={this.state.isReset}
-              />
-            );
+        return (
+          <Component
+            index={index}
+            fieldHeader={item.fieldHeader}
+            type={item.fieldtype}
+            label={item.label}
+            fieldinfo={item.fieldinfo && item.fieldinfo}
+            name={item.id}
+            id={item.id}
+            placeholder={item.placeholder}
+            description={item.description}
+            disabled={this.disabledHandler(item.id)}
+            onDisableField={this.onDisableField}
+            fieldsToDisable={item.disable}
+            value={props.values[item.id]}
+            required={item.validation && item.validation.required}
+            autoComplete={autoComplete}
+            onChange={item.fieldinfo && item.fieldinfo.typeahead ? props.setFieldValue : props.handleChange}
+            onBlur={props.handleBlur}
+            error={error}
+            touched={touched}
+            isReset={this.state.isReset}
+          />
+        );
+        return (
+          <Component
+            index={index}
+            fieldHeader={item.fieldHeader}
+            type={item.fieldtype}
+            label={item.label}
+            fieldinfo={item.fieldinfo}
+            name={item.id}
+            id={item.id}
+            placeholder={item.placeholder}
+            description={item.description}
+            dependentFields={item.dependentFields}
+            disabled={this.disabledHandler(item.id)}
+            onDisableField={this.onDisableField}
+            fieldsToDisable={item.disable}
+            value={props.values[item.id]}
+            required={item.validation.required}
+            autoComplete={autoComplete}
+            onChange={item.fieldinfo.typeahead ? props.setFieldValue : props.handleChange}
+            updateFieldData={this.updateFieldData}
+            onBlur={props.handleBlur}
+            error={error}
+            touched={touched}
+            isReset={this.state.isReset}
+          />
+        );
       }
-      return "";
+      return '';
     });
   }
 
-  componentDidMount () {
+  componentDidMount() {
     const hasDelete = this.props.formMetaData.formdef.hasDelete;
-    const mode = this.props.formData.mode
+    const mode = this.props.formData.mode;
     let isEdit = false;
-    if (mode === "Edit") {
-      isEdit = true
+    if (mode === 'Edit') {
+      isEdit = true;
     }
-    const hasDeletePermission = this.props.formProps.permissions.DELETE
+    const hasDeletePermission = this.props.formProps.permissions && this.props.formProps.permissions.DELETE;
     if (hasDelete && isEdit && hasDeletePermission) {
-      this.setState({ showDelete: true})
+      this.setState({ showDelete: true });
     }
   }
 
   render() {
-    const { formProps, tftools, recentUsage, fieldData, formMetaData, autoComplete, saveGridData } = this.props;
-    const { close, deleteRow, pgid, filter} = formProps;
+    const { formProps, tftools, recentUsage, formMetaData, autoComplete, saveGridData } = this.props;
+    const { close, deleteRow, pgid, filter } = formProps;
+    const { fieldData } = this.state;
     const fieldInfo = fieldData;
     let initialValues = {};
 
     if (this.props.filterFormData) {
-      initialValues = this.props.filterFormData
+      initialValues = this.props.filterFormData;
     }
-  
-  function handleGridRender(pgid, values, filter) {
-    renderMe(pgid, values, filter)
-  }
+
+    function handleGridRender(pgid, values, filter) {
+      renderMe(pgid, values, filter);
+    }
 
     this.displayForm = () => {
       const hasDelete = this.props.formMetaData.formdef.hasDelete;
-      const mode = this.props.formData.mode
+      const mode = this.props.formData.mode;
       let isEdit = false;
-    if (mode === "Edit") {
-      isEdit = true
-    }
-    const hasDeletePermission = this.props.formProps.permissions.DELETE
+      if (mode === 'Edit') {
+        isEdit = true;
+      }
+      const hasDeletePermission = this.props.formProps.permissions && this.props.formProps.permissions.DELETE;
 
       return (
         <Formik
@@ -168,7 +208,7 @@ class DynamicForm extends Component {
             try {
               let rowid = null;
               const mode = this.props.formData.mode;
-              if (mode === "Edit") {
+              if (mode === 'Edit') {
                 rowid = this.props.formData.index;
               }
               if (!filter) {
@@ -180,87 +220,75 @@ class DynamicForm extends Component {
               close();
               actions.resetForm({});
             } catch (error) {
-              console.log("Form Error >>>>>>  ", error);
+              console.log('Form Error >>>>>>  ', error);
               actions.setSubmitting(false);
               actions.setErrors({ submit: error.message });
             }
           }}
           onReset={() => {
-            fieldInfo && fieldInfo.forEach((item) => {
-              if (item.fieldtype == "radio" || item.fieldtyle == "checkbox") {
-                item.fieldinfo.options && item.fieldinfo.options.forEach((subItem) => {
-                  document.getElementById(subItem.id).checked = false;
-                });
-              }
-            });
+            fieldInfo &&
+              fieldInfo.forEach(item => {
+                if (item.fieldtype == 'radio' || item.fieldtyle == 'checkbox') {
+                  item.fieldinfo.options &&
+                    item.fieldinfo.options.forEach(subItem => {
+                      document.getElementById(subItem.id).checked = false;
+                    });
+                }
+              });
           }}
         >
-          {(props) => (
+          {props => (
             <Form>
               <Container>
                 <ModalBody>
                   <Form
                     onSubmit={this.props.submit}
                     style={{
-                      display: "flex",
-                      margin: "0 auto",
-                      width: "70%",
-                      flexWrap: "wrap",
+                      display: 'flex',
+                      margin: '0 auto',
+                      width: '70%',
+                      flexWrap: 'wrap'
                     }}
-                    id="myform"
+                    id='myform'
                   >
-                    <Col>
-                      {this.renderFormElements(props, fieldInfo, autoComplete)}
-                    </Col>
+                    <Col>{this.renderFormElements(props, fieldInfo, autoComplete)}</Col>
                   </Form>
                   {formMetaData.formdef && formMetaData.formdef.note && (
                     <FormGroup row>
-                    <Col sm={2} style={{marginLeft:'15px'}}>
-                      <Label for="toolsFile"></Label>
-                    </Col>
+                      <Col sm={2} style={{ marginLeft: '15px' }}>
+                        <Label for='toolsFile'></Label>
+                      </Col>
                       <Col sm={9}>
-                        <Label style={{ fontWeight: "bold" }}>
-                          {formMetaData.formdef.note}
-                        </Label>
+                        <Label style={{ fontWeight: 'bold' }}>{formMetaData.formdef.note}</Label>
                       </Col>
                     </FormGroup>
                   )}
-                  {formMetaData.formdef &&
-                    formMetaData.formdef.hasRecentUsage && (
-                      <Usage
-                        pgid={pgid}
-                        tftools={tftools}
-                        close={close}
-                        recentUsage={recentUsage}
-                      />
-                    )}
+                  {formMetaData.formdef && formMetaData.formdef.hasRecentUsage && (
+                    <Usage pgid={pgid} tftools={tftools} close={close} recentUsage={recentUsage} />
+                  )}
                 </ModalBody>
                 <ModalFooter>
-                  <Button
-                    color="primary"
-                    className="btn btn-primary"
-                    onClick={close}
-                  >
+                  <Button color='primary' className='btn btn-primary' onClick={close}>
                     Cancel
                   </Button>
                   <Button
-                    onClick={(e) => this.handleReset()}
-                    color="secondary"
-                    className="btn btn-primary mr-auto"
-                    type="reset"
+                    onClick={e => this.handleReset()}
+                    color='secondary'
+                    className='btn btn-primary mr-auto'
+                    type='reset'
                   >
-                    {" "}
-                    Reset{" "}
+                    {' '}
+                    Reset{' '}
                   </Button>
                   {this.state.showDelete && (
-                    <Button onClick={(e) => this.handleDelete()} color="danger">
-                      {" "}
-                      Delete{" "}
+                    <Button onClick={e => this.handleDelete()} color='danger'>
+                      {' '}
+                      Delete{' '}
                     </Button>
                   )}
-                  <Button type="submit" color="success">
-                    {" "}
-                    {this.props.filter   || this.props.formMetaData.griddef.isfilterform ? " View " : " Submit "}
+                  <Button type='submit' color='success'>
+                    {' '}
+                    {this.props.filter || this.props.formMetaData.griddef.isfilterform ? ' View ' : ' Submit '}
                   </Button>
                 </ModalFooter>
               </Container>
@@ -276,20 +304,18 @@ class DynamicForm extends Component {
       close();
     };
 
-    if (this.props.formData.mode == "Edit") {
+    if (this.props.formData.mode == 'Edit') {
       initialValues = this.props.formData.data;
     } else {
       fieldInfo.forEach((item, index) => {
-        initialValues[item.id] = item.value || "";
+        initialValues[item.id] = item.value || '';
       });
     }
 
     const yepSchema = fieldInfo.reduce(createYupSchema, {});
     const validateSchema = yup.object().shape(yepSchema);
 
-    return (
-      this.displayForm()
-    );
+    return this.displayForm();
   }
 }
 
--- src/lib/inputTypes/select.js
+++ src/lib/inputTypes/select.js
@@ -1,114 +1,144 @@
-import React, { Component } from "react";
-import {Input, Col, FormGroup, Label} from "reactstrap";
-import {AsyncTypeahead} from "react-bootstrap-typeahead";
-import {FieldLabel, FieldMessage, FieldHeader} from "../field";
+import React, { Component } from 'react';
+import { Input, Col, FormGroup, Label } from 'reactstrap';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { FieldLabel, FieldMessage, FieldHeader } from '../field';
 
 class CustomSelect extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        isLoading: false,
-        options: [],
-        defaultSelected: {id: '', label: ''}
-    }
+      isLoading: false,
+      options: [],
+      defaultSelected: { id: '', label: '' }
+    };
     this.handleChange = this.handleChange.bind(this);
     this.onSearchHandler = this.onSearchHandler.bind(this);
+    this.handleSelectFieldChange = this.handleSelectFieldChange.bind(this);
+    this.updateDependentField = this.updateDependentField.bind(this);
   }
 
   handleChange(selectedOptions) {
-    const {onChange, id} = this.props;
-    if(selectedOptions.length > 0) {
-      if(selectedOptions.length == 1)
-        selectedOptions = selectedOptions[0];
-    }else{
-      selectedOptions = "";
+    const { onChange, id } = this.props;
+    if (selectedOptions.length > 0) {
+      if (selectedOptions.length == 1) selectedOptions = selectedOptions[0];
+    } else {
+      selectedOptions = '';
     }
+    this.updateDependentField((selectedOptions && selectedOptions.label) || '');
     onChange(id, selectedOptions);
   }
-  
-  componentDidMount(){
-    this.setState({defaultSelected:this.props.value})
+
+  handleSelectFieldChange(event) {
+    const { onChange } = this.props;
+    this.updateDependentField(event.target.value);
+    onChange(event);
   }
 
-  onSearchHandler(query){
-    const {autoComplete, fieldinfo, id} = this.props;
-    if(fieldinfo.isasync){
-      this.setState({isLoading: true});
-      autoComplete.getAutoCompleteData(id, query)
-      .then((options) => {
-        this.setState({
-          isLoading: false,
-          options: options,
+  updateDependentField(parentSelectedValue) {
+    const { dependentFields, autoComplete, updateFieldData } = this.props;
+    if (dependentFields && dependentFields.length) {
+      dependentFields.forEach(depentFieldId => {
+        autoComplete.getAutoCompleteData(depentFieldId, parentSelectedValue).then(options => {
+          updateFieldData && updateFieldData(depentFieldId, options);
         });
       });
-    }else {
-        this.setState({options: fieldinfo.options})
     }
   }
-  
-  render() {   
-    const {defaultSelected, isLoading, options} = this.state;
-    const {name,error,touched,description,required,label,value,defaultSet,index,
-           fieldinfo,disabled,placeholder,onChange,isReset,id,fieldHeader} = this.props;
+
+  componentDidMount() {
+    this.setState({ defaultSelected: this.props.value });
+  }
+
+  onSearchHandler(query) {
+    const { autoComplete, fieldinfo, id } = this.props;
+    if (fieldinfo.isasync) {
+      this.setState({ isLoading: true });
+      autoComplete.getAutoCompleteData(id, query).then(options => {
+        this.setState({
+          isLoading: false,
+          options: options
+        });
+      });
+    } else {
+      this.setState({ options: fieldinfo.options });
+    }
+  }
+
+  render() {
+    const { defaultSelected, isLoading, options } = this.state;
+    const {
+      name,
+      error,
+      touched,
+      description,
+      required,
+      label,
+      value,
+      defaultSet,
+      index,
+      fieldinfo,
+      disabled,
+      placeholder,
+      onChange,
+      isReset,
+      id,
+      fieldHeader
+    } = this.props;
+
     if (isReset) {
-      if(defaultSelected)
-        this.typeahead && this.typeahead.getInstance()._updateSelected([defaultSelected]);
-      else
-        this.typeahead && this.typeahead.getInstance().clear(); 
+      if (defaultSelected) this.typeahead && this.typeahead.getInstance()._updateSelected([defaultSelected]);
+      else this.typeahead && this.typeahead.getInstance().clear();
     }
     return (
       <FormGroup>
         <Col>
-            {fieldHeader && <FieldHeader fieldHeader={fieldHeader} index={index} />}
-            {label && <FieldLabel label={label} required={required} />}
-            
-            {(fieldinfo && fieldinfo.typeahead) ? (
-              <AsyncTypeahead
-                id={id}
-                isLoading={isLoading}
-                labelKey={option => `${option.label ? option.label : ''}`}
-                defaultInputValue= {value && value.label? value.label :''}
-                ref={(typeahead) => this.typeahead = typeahead}
-                placeholder={placeholder}
-                onChange={this.handleChange}
-                onInputChange={this.handleInputChange}
-                disabled={disabled}
-                onSearch={this.onSearchHandler}
-                multiple={fieldinfo.multiselect}
-                error={error && touched}
-                options={options}
+          {fieldHeader && <FieldHeader fieldHeader={fieldHeader} index={index} />}
+          {label && <FieldLabel label={label} required={required} />}
+
+          {fieldinfo && fieldinfo.typeahead ? (
+            <AsyncTypeahead
+              id={id}
+              isLoading={isLoading}
+              labelKey={option => `${option.label ? option.label : ''}`}
+              defaultInputValue={value && value.label ? value.label : ''}
+              ref={typeahead => (this.typeahead = typeahead)}
+              placeholder={placeholder}
+              onChange={this.handleChange}
+              onInputChange={this.handleInputChange}
+              disabled={disabled}
+              onSearch={this.onSearchHandler}
+              multiple={fieldinfo.multiselect}
+              error={error && touched}
+              options={options}
             />
-          ):(
-              <Input
-                type="select"
-                name={name}
-                placeholder={placeholder}
-                value={value}
-                disabled={disabled}
-                onChange={onChange}
-                invalid={error && touched}
-              >
-                {!defaultSet && (
-                <option value="" disabled>
+          ) : (
+            <Input
+              type='select'
+              name={name}
+              placeholder={placeholder}
+              value={value}
+              disabled={disabled}
+              onChange={this.handleSelectFieldChange}
+              invalid={error && touched}
+            >
+              {!defaultSet && (
+                <option value='' disabled>
                   {placeholder}
                 </option>
               )}
 
-              {fieldinfo && fieldinfo.options && 
-              fieldinfo.options.map(opt => {
-                return (
-                  <option index={opt} value={opt}>
-                    {opt}
-                  </option>
-                );
-              })}
+              {fieldinfo &&
+                fieldinfo.options &&
+                fieldinfo.options.map(opt => {
+                  return (
+                    <option index={opt} value={opt}>
+                      {opt}
+                    </option>
+                  );
+                })}
             </Input>
           )}
-          <FieldMessage 
-                error={error} 
-                touched={touched} 
-                description={description} 
-          />
+          <FieldMessage error={error} touched={touched} description={description} />
         </Col>
       </FormGroup>
     );
