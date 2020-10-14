7c06e23f46b380f4dc92def6d857551cfe52ae6e
Raj Dangi <rdangi@bsihq.com>
2020-10-14 15:01:37 -0400

    Pills for child grids with parent filter added

--- src/lib/field.js
+++ src/lib/field.js
@@ -2,36 +2,50 @@ import React, { Component } from "react";
 import {Label} from "reactstrap";
 
 export class FieldMessage extends Component {
-    render() {
-        const {error,touched,description} = this.props;
-        const renderError = error && touched ? (
-            <div style={{color:'red', fontSize:15, paddingTop:4}}>{error}</div>
-        ) : null;
-        const renderDescription = description ? (
-            <div style={{color:'#33b5e5', fontSize:15, paddingTop:4}}>{description}</div>
-        ) : null;
-        return renderError?renderError:renderDescription
-    }
+  render() {
+    const { error, touched, description } = this.props;
+    const renderError =
+      error && touched ? <div style={{ color: "red", fontSize: 15, paddingTop: 4 }}>{error}</div> : null;
+    const renderDescription = description ? (
+      <div style={{ color: "#33b5e5", fontSize: 15, paddingTop: 4 }}>{description}</div>
+    ) : null;
+    return renderError ? renderError : renderDescription;
+  }
 }
 
 export class FieldLabel extends Component {
-    render() {
-        const {label, required} = this.props;
-        return (
-            <Label>{label}
-                {required && <Label style={{color:'red', fontSize: 20}}>{" *"}</Label> }
-            </Label>
-        )
-    }
+  render() {
+    const { label, required, className } = this.props;
+    return (
+      <Label className={className}>
+        {label}
+        {required && <Label style={{ color: "red", fontSize: 20 }}>{" *"}</Label>}
+      </Label>
+    );
+  }
 }
 
 export class FieldHeader extends Component {
-    render() {
-        const {fieldHeader,index} = this.props;
-        return (
-            <div style={{height: 25, fontSize: 15,borderRadius: 5, color: "#777b7e", backgroundColor: "#ebecf0", fontWeight: "bold", marginTop: index==0?25:65, marginBottom: 15, borderBottomColor: '#d3d3d3', borderBottomWidth: 1, borderBottomStyle: "solid" }}>
-                {fieldHeader}
-            </div>
-        )
-    }
+  render() {
+    const { fieldHeader, index } = this.props;
+    return (
+      <div
+        style={{
+          height: 25,
+          fontSize: 15,
+          borderRadius: 5,
+          color: "#777b7e",
+          backgroundColor: "#ebecf0",
+          fontWeight: "bold",
+          marginTop: index == 0 ? 25 : 65,
+          marginBottom: 15,
+          borderBottomColor: "#d3d3d3",
+          borderBottomWidth: 1,
+          borderBottomStyle: "solid"
+        }}
+      >
+        {fieldHeader}
+      </div>
+    );
+  }
 }
