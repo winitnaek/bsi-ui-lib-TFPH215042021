384be337205a827be29fd5422352fec54c6b8793
Vinit Naik <vnaik@bsihq.com>
2021-01-11 14:16:48 -0500

    Updated support field instead of depend on Id

--- src/lib/components/form/inputTypes/select.js
+++ src/lib/components/form/inputTypes/select.js
@@ -276,7 +276,8 @@ class CustomSelect extends Component {
         setValues(newFieldValues);
       } else {
         if (fieldinfo.fieldDisplayInfo) {
-          setFieldValue(id, selectedOption[id]);
+          let field = fieldinfo.fieldKey ? fieldinfo.fieldKey :fieldinfo.fieldDisplayInfo[0].field;
+          setFieldValue(id, selectedOption[field]);
         } else if (fieldinfo.autoPopulateFields) {
           // For single select the selectedOption is always string as per the above code
           // TODO: Check what should be the pattern for multi select and update.
