f08f021baff968c5830069d2cb46e1b44f5a017d
nigol <bsita_nigol@bsi.com>
2021-04-02 09:51:15 -0400

    UI Enhancements

--- src/lib/components/form/inputTypes/tabs.js
+++ src/lib/components/form/inputTypes/tabs.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { FormGroup, Col, Label, InputGroup, InputGroupAddon, Button } from "reactstrap";
+import { FormGroup, ButtonGroup, Button } from "reactstrap";
 import { FieldLabel, FieldMessage, FieldHeader } from "../field";
 
 class CustomTabs extends Component {
@@ -17,31 +17,26 @@ class CustomTabs extends Component {
   render() {
     const { fieldHeader, name, label, fieldinfo, required, description, hidden, error, value, touched } = this.props;
     let options = (fieldinfo && fieldinfo.options) || [];
-    let lastIndex = (options && options.length - 1) || -1;
     return (
       <FormGroup>
-        <Col className="pl-0 pr-0">
-          {fieldHeader && <FieldHeader fieldHeader={fieldHeader} index={1} />}
-          {label && <FieldLabel label={label} required={required} hidden={hidden ? "hidden" : ""} />}
-          <InputGroup id={name} hidden={hidden ? "hidden" : ""} style={{ flex: 1 }}>
-            {options.map((opt, index) => {
-              return (
-                <InputGroupAddon addonType={index == lastIndex ? "append" : "prepend"} id={opt.id} style={{ flex: 1 }}>
-                  <Button
-                    onClick={() => this.handleTabChange(name, opt.id)}
-                    outline={value == opt.id ? false : true}
-                    color={value == opt.id ? "primary" : "secondary"}
-                    style={{ flex: 1 }}
-                    className="text-center"
-                  >
-                    {opt.label}
-                  </Button>
-                </InputGroupAddon>
-              );
-            })}
-          </InputGroup>
-          <FieldMessage error={error} touched={touched} description={description} />
-        </Col>
+        {fieldHeader && <FieldHeader fieldHeader={fieldHeader} index={1} />}
+        {label && <FieldLabel label={label} required={required} hidden={hidden ? "hidden" : ""} />}
+        <ButtonGroup className="d-flex">
+          {options.map((opt) => {
+            return (
+              <Button
+                onClick={() => this.handleTabChange(name, opt.id)}
+                outline={value == opt.id ? false : true}
+                color={value == opt.id ? "info" : "secondary"}
+                style={{ flexBasis: "100%" }}
+                className="text-center"
+              >
+                {opt.label}
+              </Button>
+            );
+          })}
+        </ButtonGroup>
+        <FieldMessage error={error} touched={touched} description={description} />
       </FormGroup>
     );
   }
